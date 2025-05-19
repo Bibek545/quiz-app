79df3138d01871a4a61fd5b1007620ee87d8d064
Bibek Hanal <bbekhamal26@gmail.com>
2025-05-19 12:04:34 +1000

    added questions and welcome alert

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Welcome to the quiz")
+
+const questions = [
+    {
+        question: "What is my name?",
+        options: ["Bibek", "Hamal","Srijana","Lama"],
+        correct: 0
+    },
+    {
+        question: "What does JS stands for in programming language?",
+        options: ["Python","java","HTML","javascript"],
+        correct:3,
+    }
+]
